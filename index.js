9c60921db033f4f90f5c1298743db13bd5542df1
Lyubov Kishchenko <lyubov.mashtalir@gmail.com>
2020-09-13 19:42:11 +0300

    Create server requests for weather api

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const favicon = require('serve-favicon');
+const fetch = require('node-fetch');
 const path = require('path');
+const apiKey = "87281a4fa208ee16ddfdfdaa43a194c9";
 
 let app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
-app.get('/test', (req, res) => {
-    res.send(JSON.stringify({a: 1, b: 3}));
+app.get('/city/:name', (req, res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.name}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(function (err) {
+            console.log(`Something goes wrong. Error: ${err}`);
+        });
 });
 
+app.get('/city/:lat/:lon', (req, res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${req.params.lat}&lon=${req.params.lon}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(function (err) {
+            console.log(`Something goes wrong. Error: ${err}`);
+        });
+});
 
 app.use((request, response, next) => {
     response.status(404).send("Not Found")
